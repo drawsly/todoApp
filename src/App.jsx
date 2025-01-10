8f0094b610be3bfec1ff4f86e4f30a650a7593ee
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-01-10 20:27:59 +0300

    feat: Integrate TodoList component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import "./styles/App.css";
 import TodoCreate from "./components/TodoCreate/TodoCreate";
-import { useState } from "react";
+import TodoList from "./components/TodoList/TodoList";
 function App() {
   return (
     <div className="container">
-        <div className="todo-wrapper">
-          <div className="todo-header">
-            <h1>todo app.</h1>
-            <p>Todolarınızı yönetebileceğiniz bir uygulama.</p>
-          </div>
-          <div className="todo-create">
-            <TodoCreate />
-          </div>
-          <div className="todo-list">
-
-          </div>
+      <div className="todo-wrapper">
+        <div className="todo-header">
+          <h1>todo app.</h1>
+          <p>Todolarınızı yönetebileceğiniz bir uygulama.</p>
         </div>
+        <div className="todo-create">
+          <TodoCreate />
+        </div>
+        <div className="todo-list">
+          <TodoList />
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
