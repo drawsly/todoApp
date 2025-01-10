11186c2a4ce96a586af4c1f1319745e2f8a0bb48
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-01-10 11:33:02 +0300

    modified: renamed h1 and created todo-list wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./styles/App.css";
 import TodoCreate from "./components/TodoCreate/TodoCreate";
+import { useState } from "react";
 function App() {
   return (
     <div className="container">
         <div className="todo-wrapper">
           <div className="todo-header">
-            <h1>Todo APP</h1>
+            <h1>todo app.</h1>
             <p>Todolarınızı yönetebileceğiniz bir uygulama.</p>
           </div>
           <div className="todo-create">
             <TodoCreate />
+          </div>
+          <div className="todo-list">
+
           </div>
         </div>
     </div>
