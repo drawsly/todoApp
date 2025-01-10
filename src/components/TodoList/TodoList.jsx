ae8c75aee1c923d9728198c96d6b0fada2f9b5da
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-01-10 20:28:32 +0300

    feat: Add styling and structure to TodoList component

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import "./TodoList.css";
+import { FiEdit } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 function TodoList() {
   return (
-    <div>TodoList</div>
-  )
+    <div className="todolist-wrapper">
+      <div className="list-item">
+        <span className="list-text">Deneme yazısı.</span>
+        <div className="button-group">
+          <FiEdit style={{ color: "black" }} className="circle-btn" />
+          <FiX style={{ color: "red" }} className="circle-btn" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
