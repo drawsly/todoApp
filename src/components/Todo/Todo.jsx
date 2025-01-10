a76b54d10cf64be0920d9b7ed3815bbfd6abbcc9
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-01-10 21:27:39 +0300

    fix: Destructure todo object in Todo component for cleaner code

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,9 +3,11 @@ import { FiEdit } from "react-icons/fi";
 import { FiX } from "react-icons/fi";
 
 function Todo({ todo }) {
+  const { id, text } = todo;
+
   return (
     <div className="list-item">
-      <span className="list-text">{todo.text}</span>
+      <span className="list-text">{text}</span>
       <div className="button-group">
         <FiEdit style={{ color: "black" }} className="circle-btn" />
         <FiX style={{ color: "red" }} className="circle-btn" />
